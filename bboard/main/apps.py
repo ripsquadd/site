27458275db88e312c43816ca46d0acd1b4492364
Григорий Гунько <gr413_gugal@lab-404-10.tomtit-tomsk.ru>
2022-09-14 14:41:14 +0700

    Initial commit

---
+++ bboard/main/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class MainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    verbose_name = 'Электронная доска объявлений'
+    name = 'main'
+
+
---
+++ bboard/main/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from django.dispatch import Signal
+
+from .utilities import send_activation_notification
+
+user_registrated = Signal(providing_args=['instance'])
+
+
+def user_registrated_dispatcher(sender, **kwargs):
+    send_activation_notification(kwargs['instance'])
+
+
+user_registrated.connect(user_registrated_dispatcher)
+
+class AdvUser(AbstractUser):
+    is_activated = models.BooleanField(default=True, db_index=True,
+                                       verbose_name='Прошел активацию?')
+    send_messages = models.BooleanField(default=True,
+                                        verbose_name='Оповещать при новых комментариях?')
+
+    class Meta(AbstractUser.Meta):
+        pass
+
+class Rubric(models.Model):
+   name = models.CharField(max_length=20, db_index=True, unique=True,
+                           verbose_name='Название')
+   order = models.SmallIntegerField(default=0, db_index=True,
+                                    verbose_name='Порядок')
+   super_rubric = models.ForeignKey('SuperRubric',
+                                    on_delete=models.PROTECT, null=True, blank=True,
+                                    verbose_name='Надрубрика')
+
+class SuperRubricManager(models.Manager):
+   def get_queryset(self):
+       return super().get_queryset().filter(super_rubric__isnull=True)
+
+
+class SuperRubric(Rubric):
+   object = SuperRubricManager()
+
+   def __str__(self):
+       return self.name
+
+   class Meta:
+       proxy = True
+       ordering = ('order', 'name')
+       verbose_name = 'Надрубрика'
+       verbose_name_plural = 'Надрубрики'
+
+class SubRubricManager(models.Manager):
+   def get_queryset(self):
+       return super().get_queryset().filter(super_rubric__isnull=False)
+
+
+class SubRubric(Rubric):
+   object = SubRubricManager()
+
+   def __str__(self):
+       return '%s - %s' % (self.super_rubric, self.name)
+
+   class Meta:
+       proxy = True
+       ordering = ('super_rubric__order', 'super_rubric__name', 'order', 'name')
+       verbose_name = 'Подрубрика'
+       verbose_name_plural = 'Подрубрики'
